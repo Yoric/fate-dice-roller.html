e59b7829822d4415e4ed40d6faa19452bb6bec93
David Teller <D.O.Teller@gmail.com>
2019-07-15 21:41:33 +0200

    [Test] FirefoxOS install

--- www/js/index.js
+++ www/js/index.js
@@ -192,7 +192,7 @@ var app = {
         alert("...letting the request fly");
         setTimeout(function() {
             alert("One second later");
-            alert("Result: " + JSON.stringify(request.result));
+            alert("Result: " + JSON.stringify(request));
         }, 1000);
     },
 
